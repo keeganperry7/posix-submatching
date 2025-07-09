f55baa315846756ab6eb9b303bc91a8fa6508b1d
keeganperry7 <keegan.perry.kp@gmail.com>
2025-07-09 10:16:15 +0200

    Replace #eval checks with #guard

--- PosixSubmatching/Examples.lean
+++ PosixSubmatching/Examples.lean
@@ -9,34 +9,17 @@ instance : Coe Char (Regex Char) where
 
 -- (a*)* · b*
 def r₁ : Regex Char := (group "0" [] (star 'a')).star.mul (star 'b')
-#eval r₁.captures "bb".toList
-#eval r₁.captures' "bb".toList
-
-#eval r₁.captures "aabb".toList
-#eval r₁.captures' "aabb".toList
+#guard r₁.captures "bb".toList = r₁.captures' "bb".toList
+#guard r₁.captures "aabb".toList = r₁.captures' "aabb".toList
 
 -- (a + aa)*
 def r₂ : Regex Char := (group "0" [] (plus 'a' (mul 'a' 'a'))).star
-#eval r₂.captures []
-#eval r₂.captures' []
-
-#eval r₂.captures "aaa".toList
-#eval r₂.captures' "aaa".toList
-
-#eval r₂.captures "aaaa".toList
-#eval r₂.captures' "aaaa".toList
+#guard r₂.captures [] = r₂.captures' []
+#guard r₂.captures "aaa".toList = r₂.captures' "aaa".toList
+#guard r₂.captures "aaaa".toList = r₂.captures' "aaaa".toList
 
 -- (a + ab)(b + ε)
 def r₃ : Regex Char := (group "0" [] (plus 'a' (mul 'a' 'b'))).mul (group "1" [] (plus 'b' epsilon))
-#eval r₃.captures "ab".toList
-#eval r₃.captures' "ab".toList
-
-#eval r₃.captures "abb".toList
-#eval r₃.captures' "abb".toList
-
-#eval r₃.captures "a".toList
-#eval r₃.captures' "a".toList
-
--- ε · a
-def r₄ : Regex Char := mul epsilon 'a'
-#eval r₄.parse "a".toList
+#guard r₃.captures "ab".toList = r₃.captures' "ab".toList
+#guard r₃.captures "abb".toList = r₃.captures' "abb".toList
+#guard r₃.captures "a".toList = r₃.captures' "a".toList
